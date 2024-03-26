71aedc9274c88f3057af7138061758bdf2ae6b96
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-26 17:20:05 +0900

    Fix: アイコンコンポーネントをマウントするロジックを修正 #6

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -4,26 +4,32 @@ import "./controllers"
 import React from "react";
 import { createRoot } from "react-dom/client";
 import MenuIcon from "./components/header/MenuIcon";
+import BellIcon from "./components/header/BellIcon";
 
-function mountMenuIcon() {
-  const mountNode = document.getElementById('menu-icon');
-  const root = createRoot(mountNode);
-  root.render(<MenuIcon />);
+function icon(target_id, Component) {
+  function mountIcon() {
+    const mountNode = document.getElementById(target_id);
+    const root = createRoot(mountNode);
+    root.render(<Component />);
+  }
+
+  function checkIcon() {
+    return document.querySelector(`#${target_id} svg`);
+  }
+  
+  document.addEventListener('turbo:render', () => {
+    if (!checkIcon()) {
+      mountIcon();
+    }
+  });
+  
+  document.addEventListener('turbo:load', async () => {
+    await new Promise((resolve) => { setTimeout(resolve, 0); }); // turbo:renderの発火による描写ラグを考慮。重複のレンダリング防止
+    if (!checkIcon()) {
+      mountIcon();
+    }
+  });
 }
 
-function checkMenuIcon() {
-  return document.querySelector('#menu-icon svg');
-}
-
-document.addEventListener('turbo:render', () => {
-  if (!checkMenuIcon()) {
-    mountMenuIcon();
-  }
-});
-
-document.addEventListener('turbo:load', async () => {
-  await new Promise((resolve) => { setTimeout(resolve, 0); }); // turbo:renderの発火による描写ラグを考慮
-  if (!checkMenuIcon()) {
-    mountMenuIcon();
-  }
-});
+icon('menu-icon', MenuIcon);
+icon('notification', BellIcon);
