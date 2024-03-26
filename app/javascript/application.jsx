0bea954862ac273ca8a340f8d1ed10d31688e461
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-26 21:53:14 +0900

    Fix: アイコンコンポーネントをレンダーする対象の有無でcreateRootするかどうか条件を追加（ログイン前後でヘッダーのhtmlが変化するのでエラーが起きないよう対応） #6

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -7,29 +7,31 @@ import MenuIcon from "./components/header/MenuIcon";
 import BellIcon from "./components/header/BellIcon";
 
 function icon(target_id, Component) {
-  function mountIcon() {
-    const mountNode = document.getElementById(target_id);
-    const root = createRoot(mountNode);
-    root.render(<Component />);
-  }
-
-  function checkIcon() {
-    return document.querySelector(`#${target_id} svg`);
-  }
-  
-  document.addEventListener('turbo:render', () => {
-    if (!checkIcon()) {
-      mountIcon();
+  const mountNode = document.getElementById(target_id);
+  if (mountNode) {
+    function mountIcon() {  
+      const root = createRoot(mountNode);
+      root.render(<Component />);
     }
-  });
   
-  document.addEventListener('turbo:load', async () => {
-    await new Promise((resolve) => { setTimeout(resolve, 0); }); // turbo:renderの発火による描写ラグを考慮。重複のレンダリング防止
-    if (!checkIcon()) {
-      mountIcon();
+    function checkIcon() {
+      return document.querySelector(`#${target_id} svg`);
     }
-  });
-}
+    
+    document.addEventListener('turbo:render', () => {
+      if (!checkIcon()) {
+        mountIcon();
+      }
+    });
+    
+    document.addEventListener('turbo:load', async () => {
+      await new Promise((resolve) => { setTimeout(resolve, 0); }); // turbo:renderの発火による描写ラグを考慮。重複のレンダリング防止
+      if (!checkIcon()) {
+        mountIcon();
+      }
+    });  
+  }
+};
 
 icon('menu-icon', MenuIcon);
 icon('notification', BellIcon);
