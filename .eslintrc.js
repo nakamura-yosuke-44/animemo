85869bf5f0132ac2a738433046fabc5653e5bee7
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-28 17:20:59 +0900

    Add:     eslint_plugin_tailwindcssをインストール。     eslintの設定ファイルを編集。     pluginをインストールしてeslintを実行すると再度、Error: require() of ES Module /Users/nakamurayousuke/dev/kodoguru/node_modules/strip-ansi/index.js from /Users/nakamurayousuke/dev/kodoguru/node_modules/eslint/lib/cli-engine/formatters/stylish.js not supported.のエラーが出たので、yarn.lockを削除し、yarn installしたところエラー解消。 #16

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'airbnb',
+      'plugin:tailwindcss/recommended',
+    ],
+    plugins: [
+    'tailwindcss',
+    ],
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: [
+          '.eslintrc.{js,cjs}',
+        ],
+        parserOptions: {
+          sourceType: 'script',
+        },
+      },
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-plusplus': 'warn',
+      'react/jsx-props-no-spreading': 'warn',
+      'jsx-a11y/no-static-element-interactions': 'warn',
+      'jsx-a11y/click-events-have-key-events': 'warn',
+    }
+  };
+  
