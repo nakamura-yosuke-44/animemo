782a8103a074d300ef4076fd5fd6a616628c6c44
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-31 23:12:44 +0900

    Add: ユーザーモデルに、プライバシーポリシーと利用規約に同意の有無を判定する、agreement_terms属性をboolean型で追加 createアクション時にagreement_rermsがtrueであるか判定するバリデーションを設定 アカウント作成時にagreement_termsの値を受け取れるようにコントローラーで設定追加 #7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_29_014647) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_31_075045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_29_014647) do
     t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "agreement_terms", default: false, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
