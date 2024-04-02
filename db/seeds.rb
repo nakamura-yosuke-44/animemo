01c7c89ba11eee953a8fae3fd56e0cb9bbc505b9
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-04-02 21:16:17 +0900

    Add: csvファイルをの読み込みコードをseedに追加。 データベースにデータを挿入。 #10

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "csv"
+
+CSV.foreach(Rails.root.join('db', 'shops.csv'), encoding: "bom|utf-8", headers: :first_row) do |data|
+  Shop.create(
+    name: data['name'], 
+    kana: data['kana'], 
+    food: data['food'], 
+    map: data['map'], 
+    map_iflame: data['map_iflame'], 
+    prefecture: data['prefecture'], 
+    municipalities: data['municipalities'], 
+    stree_address: data['stree_address'],
+    station: data['station'], 
+    transportation: data['transportation'], 
+    phone_number: data['phone_number'],
+    hp: data['hp'], 
+    tabelog: data['tabelog'], 
+    latitude: data['latitude'], 
+    longitude: data['longitude']
+  )
+end
