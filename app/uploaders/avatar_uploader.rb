0df80102f6e0c538ef16d9fbb5fd623a32da895f
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-04-15 11:23:51 +0900

    Fix: carrierwaveのリサイズ設定を変更

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -10,7 +10,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     storage :file
   end
   # storage :fog
-  process resize_to_fit: [400, 300]
+  process resize_to_fill: [320, 240, 'Center']
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
