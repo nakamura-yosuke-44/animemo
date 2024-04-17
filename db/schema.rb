4ffd3215a0c3c40d0ddb0cd2a782cfee3b0189c9
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-04-17 21:18:45 +0900

    Add: follower_idとfollowed_idにunique制約を追加 #39

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_16_071248) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_121451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_16_071248) do
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   end
 
   create_table "shops", force: :cascade do |t|
