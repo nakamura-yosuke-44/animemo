607a94ac614aeaed1d84c02f5bbab5863ae9766f
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-04-20 06:42:14 +0900

    fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_view.preload_links_header = false
-  config.action_mailer.default_url_options = { protocol: 'https', ENV["MAILERTOGO_SMTP_HOST"]}
+  config.action_mailer.default_url_options = { protocol: 'https', host: ENV["MAILERTOGO_SMTP_HOST"] }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   
