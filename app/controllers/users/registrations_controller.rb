b18fea9f54e9f3da3b981f62e3dad5620f484ed4
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-04-19 10:05:03 +0900

    Add: ユーザー情報編集ページを作成。 deviseの設定微修正 #61

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,15 +42,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #  devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
