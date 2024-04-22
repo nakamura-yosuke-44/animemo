9122dbbbe7c417d8703a61129e536a47322f3016
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-04-22 11:11:49 +0900

    fix omniauth config.omniauth :twitter

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,8 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], token_params: { parse: :json }
-
+  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], callback_url: ENV['TWITTER_API_CALLBACK_URL']
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
