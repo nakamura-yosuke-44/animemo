02abb9adc6a975d04957281bc394c4932e1bb2c7
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-04-22 12:47:36 +0900

    fix: twitter認証の設定を修正

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,13 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token, only: :twitter
   def twitter
-    # 認証データを元にデータベースでユーザーを探し、なければ作る。
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted? # ユーザーがデータベース上に存在している。
-      sign_in_and_redirect @user # ユーザーをsign_inする
+    if @user.persisted?
+      sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: __callee__.to_s.capitalize) if is_navigational_format?
-    else # 何らかの理由でデータベースに保存されていない。
-      session["devise.user_attributes"] = @user.attributes # 認証データを覚えておく。
-      redirect_to new_user_registration_url(from_omniauth_callback: "１") # ユーザーを新規登録ページに転送。
+    else
+      session["devise.user_attributes"] = @user.attributes
+      redirect_to new_user_registration_url(from_omniauth_callback: "１")
     end
   end
 end
